9b5733c048083e940cde84752c0c6d7d88f505d6
Seonggil Jeong <sgjeong1108@icloud.com>
2025-12-22 01:21:23 +0900

    feat: split Airflow service profiles and add scripts for DAG processor and triggerer

---
+++ scripts/run_dag_processor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+
+
+echo "=== Stop Docker Container (dag-processor, init) ==="
+docker ps -a -q --filter "name=airflow-dag-processor" \
+--filter "name=airflow-init" | xargs -r sudo docker stop || true
+echo "=== Remove Docker Container (dag-processor, init) ==="
+docker ps -a -q --filter "name=airflow-dag-processor" \
+--filter "name=airflow-init" | xargs -r sudo docker rm || true
+
+echo "=== Ensuring Airflow DAG Processor is up and running ==="
+docker-compose \
+  -f ../docker-compose-prod.yaml \
+  --env-file ../.env.prod \
+  --profile dag-processor \
+  up -d --remove-orphans
--- scripts/run_scheduler.sh
+++ scripts/run_scheduler.sh
@@ -3,15 +3,11 @@ set -e
 cd "$(dirname "$0")"
 
 
-echo "=== Stop Docker Container (scheduler, dag-processor, triggerer, init) ==="
-docker ps -a -q --filter "name=airflow-dag-processor" \
---filter "name=airflow-triggerer" \
---filter "name=airflow-scheduler" \
+echo "=== Stop Docker Container (scheduler, init) ==="
+docker ps -a -q --filter "name=airflow-scheduler" \
 --filter "name=airflow-init" | xargs -r sudo docker stop || true
-echo "=== Remove Docker Container (scheduler, dag-processor, triggerer, init) ==="
-docker ps -a -q --filter "name=airflow-dag-processor" \
---filter "name=airflow-triggerer" \
---filter "name=airflow-scheduler" \
+echo "=== Remove Docker Container (scheduler, init) ==="
+docker ps -a -q --filter "name=airflow-scheduler" \
 --filter "name=airflow-init" | xargs -r sudo docker rm || true
 
 echo "=== Ensuring Airflow Scheduler is up and running ==="
