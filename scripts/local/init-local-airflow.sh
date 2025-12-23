c9153dae90852dba20a4a45b8f96dd98aae5e552
Seonggil Jeong <sgjeong1108@icloud.com>
2025-12-23 20:27:47 +0900

    refactor: add build option to scripts to troubleshoot pip install

--- scripts/local/init-local-airflow.sh
+++ scripts/local/init-local-airflow.sh
@@ -4,4 +4,4 @@ set -e
 docker-compose \
   -f ../../docker-compose-local.yaml \
   --env-file ../../.env.local \
-  up -d --build --force-recreate --remove-orphans
+  up -d --build --force-recreate --remove-orphans --build
