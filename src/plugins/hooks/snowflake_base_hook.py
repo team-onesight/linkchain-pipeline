43be266b59d25aafb8ce41977b6b351f6d77e8a4
minguk-cucu <minkuk0118@gmail.com>
2025-12-23 10:12:39 +0900

    feat: add snowflake hooks
    pyproject.toml : snowflake 연결 library 추가
    ./src/plugins/hooks/snowflake_base_hook.py : snowflake 연결 base
    
    ./src/plugins/ho
    oks/snowflake_command_hook.py : snowflake command hook
    
    ./src/plugins/hooks/snowflake_raw_data_hook.py : snowflake raw_data hook
    
    ./src/plugins/hooks/snowflake_analytics_hook.py : snowflake analytics hook
    
    './src/plugins/hooks/snowflake_ods_hook.py : snowflake ods hook'

---
+++ src/plugins/hooks/snowflake_base_hook.py
@@ -0,0 +1,36 @@
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+class CustomSnowflakeBaseHook(SnowflakeHook):
+    """
+    CustomSnowflakeBaseHook은 snowflake 데이터베이스에 연결하기 위한 커스텀 베이스 훅입니다.
+        
+    :param snowflake_conn_id: The Airflow connection ID to use for Snowflake.
+    :param database: The database to use for the session.
+    :param schema: The schema to use for the session.
+    """
+
+    def __init__(self, 
+                 snowflake_conn_id: str = "snowflake_conn", 
+                 database: str = None, 
+                 schema: str = None, 
+                 *args, **kwargs):
+        super().__init__(snowflake_conn_id=snowflake_conn_id, *args, **kwargs)
+        self.database = database
+        self.schema = schema
+
+    def get_conn(self):
+        """
+        get connection
+        """
+        conn = super().get_conn()
+
+        if self.database or self.schema:
+            with conn.cursor() as cur:
+                if self.database:
+                    self.log.info(f"Setting session database to: {self.database}")
+                    cur.execute(f"USE DATABASE {self.database}")
+                if self.schema:
+                    self.log.info(f"Setting session schema to: {self.schema}")
+                    cur.execute(f"USE SCHEMA {self.schema}")
+        
+        return conn
---
+++ src/plugins/hooks/snowflake_command_hook.py
@@ -0,0 +1,37 @@
+from plugins.hooks.snowflake_base_hook import CustomSnowflakeBaseHook
+
+class SnowflakeCommandHook(CustomSnowflakeBaseHook):
+    """
+    SnowflakeCommandHook은 snowflake 데이터베이스에 command를 실행하기 위한 커스텀 훅입니다.
+    INSERT, COPY INTO 등 결과를 반환하지 않거나 multi-schema를 다뤄야 하는 쿼리를 실행할 때 사용하시기 바랍니다.
+
+    :param snowflake_conn_id: The Airflow connection ID to use for Snowflake.
+    :param database: The database to use for the session.
+    :param schema: The schema to use for the session.
+    """
+
+    def __init__(self, 
+                 snowflake_conn_id: str = "snowflake_conn", 
+                 database: str = None, 
+                 schema: str = None, 
+                 *args, **kwargs):
+        super().__init__(snowflake_conn_id=snowflake_conn_id, 
+                         database=database,
+                         schema=schema,
+                         *args, **kwargs)
+
+    def query(self, sql: str) -> list:
+        """
+        SQL command를 실행합니다.
+        
+        :param sql: 실행할 쿼리
+        :type sql: str
+        :return: 쿼리 결과
+        :rtype: list
+        """
+        conn = self.get_conn()
+        with conn.cursor() as cur:
+            self.log.info(f"Executing query: {sql}")
+            cur.execute(sql)
+            results = cur.fetchall()
+        return results
