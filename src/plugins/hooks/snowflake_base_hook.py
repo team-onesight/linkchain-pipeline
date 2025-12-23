35ece676a1f20902cc1f7a045da386699479c444
minguk-cucu <minkuk0118@gmail.com>
2025-12-23 10:42:43 +0900

    feat : add self conn field and declare SnowflakeConnection type
    base_hook 내부에 conn을 정의하고 또 다시 같은 요청이 들어왔을 때 이미 존재한다면 존재하는 conn을 반환합니다.
    또한 SnowflakeConnection 반환 객체를 명시했습니다.

--- src/plugins/hooks/snowflake_base_hook.py
+++ src/plugins/hooks/snowflake_base_hook.py
@@ -1,5 +1,5 @@
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
-
+from snowflake.connector import SnowflakeConnection
 class CustomSnowflakeBaseHook(SnowflakeHook):
     """
     CustomSnowflakeBaseHook은 snowflake 데이터베이스에 연결하기 위한 커스텀 베이스 훅입니다.
@@ -8,6 +8,7 @@ class CustomSnowflakeBaseHook(SnowflakeHook):
     :param database: The database to use for the session.
     :param schema: The schema to use for the session.
     """
+    conn : SnowflakeConnection = None
 
     def __init__(self, 
                  snowflake_conn_id: str = "snowflake_conn", 
@@ -18,19 +19,22 @@ class CustomSnowflakeBaseHook(SnowflakeHook):
         self.database = database
         self.schema = schema
 
-    def get_conn(self):
+    def get_conn(self) -> SnowflakeConnection:
         """
         get connection
         """
-        conn = super().get_conn()
+        if self.conn:
+            return self.conn
+        else :
+            conn = super().get_conn()
 
-        if self.database or self.schema:
-            with conn.cursor() as cur:
-                if self.database:
-                    self.log.info(f"Setting session database to: {self.database}")
-                    cur.execute(f"USE DATABASE {self.database}")
-                if self.schema:
-                    self.log.info(f"Setting session schema to: {self.schema}")
-                    cur.execute(f"USE SCHEMA {self.schema}")
+            if self.database or self.schema:
+                with conn.cursor() as cur:
+                    if self.database:
+                        self.log.info(f"Setting session database to: {self.database}")
+                        cur.execute(f"USE DATABASE {self.database}")
+                    if self.schema:
+                        self.log.info(f"Setting session schema to: {self.schema}")
+                        cur.execute(f"USE SCHEMA {self.schema}")
         
         return conn
