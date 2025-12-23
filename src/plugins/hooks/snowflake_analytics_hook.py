f5c5cc56f580940f9f8ab5760fcd1d600abf3623
Kim, Min-guk <74689888+minguk-cucu@users.noreply.github.com>
2025-12-23 10:48:42 +0900

    feat: s3, snowflake hook 생성 (#6)
    * feat: add s3_hook
    
    * feat: add snowflake hooks
    
    pyproject.toml : snowflake 연결 library 추가
    ./src/plugins/hooks/snowflake_base_hook.py : snowflake 연결 base
    
    ./src/plugins/ho
    oks/snowflake_command_hook.py : snowflake command hook
    
    ./src/plugins/hooks/snowflake_raw_data_hook.py : snowflake raw_data hook
    
    ./src/plugins/hooks/snowflake_analytics_hook.py : snowflake analytics hook
    
    './src/plugins/hooks/snowflake_ods_hook.py : snowflake ods hook'
    
    * fix : change method name
    
    query -> command . for CQRS pattern
    
    * feat : add self conn field and declare SnowflakeConnection type
    
    base_hook 내부에 conn을 정의하고 또 다시 같은 요청이 들어왔을 때 이미 존재한다면 존재하는 conn을 반환합니다.
    또한 SnowflakeConnection 반환 객체를 명시했습니다.
    
    * feat : add ABC attribute to CustomSnowflakeBaseHook
    
    상속받아서 사용해야만 하는 객체이기 때문에 분리했습니다.

---
+++ src/plugins/hooks/snowflake_analytics_hook.py
@@ -0,0 +1,17 @@
+from plugins.hooks.snowflake_base_hook import CustomSnowflakeBaseHook
+
+class SnowflakeAnalyticsQueryHook(CustomSnowflakeBaseHook):
+    """
+    SnowflakeAnalyticsQueryHook은 snowflake 데이터베이스에 Analytics 스키마를 로드하기 위한 커스텀 훅입니다.
+    SELECT 쿼리 등을 실행하고 결과를 받을 때 사용하시기 바랍니다.
+        
+    :param snowflake_conn_id: The Airflow connection ID to use for Snowflake.
+    """
+
+    def __init__(self, 
+                 snowflake_conn_id: str = "snowflake_conn",
+                 *args, **kwargs):
+        super().__init__(snowflake_conn_id=snowflake_conn_id, 
+                         database="LINKCHAIN", 
+                         schema="ANALYTICS", 
+                         *args, **kwargs)
