ff41a4005c12e5bf54352aeed703c5a4c8f698d6
SeongJin LEE <86252835+seongjin33hj@users.noreply.github.com>
2025-12-26 20:12:03 +0900

    feat: OLAP -> OLTP 파이프라인 (OLTP target table: link, link_group, tag)  (#17)
    * feat: add method -> get_olap_table_data
    
    * feat: add method 'insert_rows', 'upsert_table'
    
    * fix: add truncate login on staging table
    
    * feat: OLAP -> OLTP task operator 작성
    
    * feat: add DAG and SQL for OLAP to OLTP ETL
    
    * fix: remove top-level variable -> fetch variable dynamically by group_id
    
    * chore: 통일된 dag_id 값으로 변경 (olap_to_oltp_all -> olap_to_oltp_dag)
    
    * chore: change postgres_conn_id, snowflake_conn_id -> postgres_default, snowflake_default

---
+++ src/plugins/operators/staging_to_target_operator.py
@@ -0,0 +1,16 @@
+from airflow.models import BaseOperator
+from hooks.postgres_olap_to_oltp_hook import PostgresOlapToOltpHook
+
+
+class StagingToTargetOperator(BaseOperator):
+    """
+    staging.{name} → public.{name} 테이블로 upsert하는 Operator
+    """
+
+    def __init__(self, upsert_sql, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.upsert_sql = upsert_sql
+
+    def execute(self, context) -> None:
+        pg_hook = PostgresOlapToOltpHook(postgres_conn_id="postgres_default")
+        pg_hook.upsert_table(self.upsert_sql)
