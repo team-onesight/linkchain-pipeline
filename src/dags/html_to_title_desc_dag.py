1ff4f56042fbc8ef2e67b5c2dafd7a98f6cf7fde
SteveKimSR <kimsr96@gmail.com>
2025-12-29 16:59:22 +0900

    use S3 instead of using XCom

--- src/dags/html_to_title_desc_dag.py
+++ src/dags/html_to_title_desc_dag.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import json
 
 import pandas as pd
 from airflow.sdk import DAG, task
@@ -17,20 +18,36 @@ def merge_combined_sources_to_integrated_table() -> int:
     return count
 
 @task
-def get_urls_without_title_desc() -> list:
+def get_urls_without_title_desc(**context) -> list:
     """Title/Desc가 NULL인 대상 목록 가져오기"""
     snowflake_hook = SnowflakeCommandHook()
     urls = snowflake_hook.get_integrated_table_where_title_desc_is_null()
     link_id_with_s3_path = [(row[0], row[1]) for row in urls]
+
+    s3hook = S3Hook(bucket_name='de7-team1')
+    ds_nodash = context['ds_nodash']
+    bytes_data = json.dumps(link_id_with_s3_path).encode('utf-8')
+    tmp_key_path = f'tmp/link_id_with_s3_path_{ds_nodash}.json'
+
+    s3hook.upload_bytes(
+        bytes_data=bytes_data,
+        key=tmp_key_path,
+        replace=True
+    )
+
     logging.info(
         f'{len(link_id_with_s3_path)} links looking for title and description'
     )
-    return link_id_with_s3_path
+    logging.info(f'tmp file saved on: {tmp_key_path}')
+    return tmp_key_path
 
 @task
-def extract_title_desc(link_id_with_s3_path:list)-> list:
+def extract_title_desc(tmp_key_path: str, **context)-> list:
     """S3에서 HTML 다운로드 및 정보 추출"""
     s3hook = S3Hook(bucket_name='de7-team1')
+    json_string = s3hook.download_bytes(tmp_key_path)
+    link_id_with_s3_path = json.loads(json_string)
+
     link_id_with_title_desc = []
     start = time.time()
     logging.info(
@@ -43,17 +60,31 @@ def extract_title_desc(link_id_with_s3_path:list)-> list:
         if title is not None:
             link_id_with_title_desc.append((title, description, link_id))
 
+    bytes_data = json.dumps(link_id_with_title_desc).encode('utf-8')
+    ds_nodash = context['ds_nodash']
+    tmp_key_path = f'tmp/title_desc_url_{ds_nodash}.json'
+
+    s3hook.upload_bytes(
+        bytes_data=bytes_data,
+        key=tmp_key_path,
+        replace=True
+    )
+
     logging.info(f'{len(link_id_with_title_desc)} data waiting for update')
     logging.info(
         f'excluded {len(link_id_with_s3_path)-len(link_id_with_title_desc)} data'
     )
     logging.info(f'execute time : {time.time() - start:.2f}s')
-
-    return link_id_with_title_desc
+    logging.info(f'tmp file saved on: {tmp_key_path}')
+    return tmp_key_path
 
 @task
-def update_to_integrated_table(formatted_data:pd.DataFrame) -> int:
+def update_to_integrated_table(tmp_key_path: str) -> int:
     """최종 테이블 업데이트"""
+    s3hook = S3Hook(bucket_name='de7-team1')
+    json_string = s3hook.download_bytes(tmp_key_path)
+    formatted_data = json.loads(json_string)
+
     sql = """
         UPDATE LINKCHAIN.ANALYTICS.INTEGRATED_TABLE
         SET
