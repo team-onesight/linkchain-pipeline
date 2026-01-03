871c1e6ced9a130bfc42cbf1c3bbf1a4cb1eafba
seon99il <sgjeong1108@icloud.com>
2026-01-04 06:01:16 +0900

    feat: add fct_daily_link_counts model and related DAG for daily link statistics

---
+++ src/dags/dbt_dbt_daily_link_stats_dag.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+from cosmos import (
+    DbtDag,
+    ExecutionConfig,
+    ExecutionMode,
+    ProfileConfig,
+    ProjectConfig,
+    RenderConfig,
+)
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+
+DBT_PROJECT_PATH = "/opt/airflow/dags/dbt/linkchain"
+DBT_EXECUTABLE_PATH = "/opt/airflow/dbt_venv/bin/dbt"
+
+project_config = ProjectConfig(dbt_project_path=DBT_PROJECT_PATH)
+
+profile_config = ProfileConfig(
+    profile_name="linkchain",
+    target_name="prod",
+    profile_mapping=SnowflakeUserPasswordProfileMapping(conn_id="snowflake_default"),
+)
+
+execution_config = ExecutionConfig(
+    dbt_executable_path=DBT_EXECUTABLE_PATH,
+    execution_mode=ExecutionMode.LOCAL,
+)
+
+
+dbt_daily_link_stats = DbtDag(
+    project_config=project_config,
+    profile_config=profile_config,
+    execution_config=execution_config,
+    dag_id="dbt_daily_link_stats",
+    start_date=datetime(2025, 12, 1),
+    schedule="@daily",
+    catchup=False,
+    tags={"linkchain", "stats", "marts"},
+    operator_args={
+        "vars": {"check_date": "{{ ds }}"},
+    },
+    render_config=RenderConfig(
+        select=["+fct_daily_link_counts"],
+    ),
+)
