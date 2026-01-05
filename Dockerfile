ff0c8a9cc561ea131badec27309d6cdd60d2e642
Steve Kim <54872857+SteveKimSR@users.noreply.github.com>
2026-01-05 16:16:04 +0900

    Feat extract tag (#39)
    * docs: PR template 추가 및 랜덤 리뷰어 생성 기능 추가
    
    * fix: Airflow CD Workflow
    
    * feat: Add script to clear Airflow Docker containers and integrate with deployment playbook
    
    * fix: ignore airflow.cfg
    
    * fix: update Dockerfile to set ownership for pyproject.toml and improve installation process (#10) (#11)
    
    * deploy (#18)
    
    * Feature/oltp to olap (#14)
    
    * feat : postgreSQL fetch logic
    
    postgreSQL에서 지정된 테이블에 맞는 데이터를 가져오는 메소드를 hook내에 정의했습니다.
    
    * feat: postgreSQL to snowflake operator
    
    postgreSQL의 테이블들을 Snowflake의 ods schema에 적재하는 operator을 만들었습니다.
    
    * feat: oltp_to_olap_dag
    
    postgreSQL에서 Snowflake에 적재하는 operator을 실행하는 DAG을 작성했습니다.
    
    * feat: add error handling for crawl failures in fetch_and_save_html_operator (#16)
    
    * refactor: implement base crawler architecture and refactor existing crawlers (#15)
    
    * fix: enhance cleanup script to remove unused Docker containers and build cache
    
    fix: update fetch_html_and_save_dag schedule to every 20m
    
    * refactor: implement base crawler architecture and refactor existing crawlers
    
    * refactor: add links_xcom_key parameter to SnowflakeUpsertLinksOperator for improved flexibility
    
    ---------
    
    Co-authored-by: Kim, Min-guk <74689888+minguk-cucu@users.noreply.github.com>
    
    * deploy (#35)
    
    * feat: initialize dbt project structure with models and configurations (#32)
    
    * Feature/calculate user embedding dag : link_embedding 값을 이용한 user_embedding 계산 pipeline 구현 (#33)
    
    * feat: add postgres_transactional_hook for making operators
    
    * feat: add operator for user_embedding dag
    
    * feat: add user_embedding_pipeline_dag
    
    * fix: rebase uv.lock
    
    * chore: ruff linting
    
    * feat: impl upsert link group method (aggregate_links_dag) (#34)
    
    * refactor: rename DAG file and update tags for clarity
    
    * feat: implement merge_link_groups method for deduplicating and loading link group data
    
    * feat: add URL matching and composite strategy for filtering links
    
    ---------
    
    Co-authored-by: SeongJin LEE <86252835+seongjin33hj@users.noreply.github.com>
    
    * hotfix: fix circular dependency
    
    * re commit
    
    * feature 구현, mecab 라이브러리 미설치? 이슈
    
    * python 파일 이름 변경
    
    * feature 구현
    
    * fix ruff
    
    ---------
    
    Co-authored-by: Seonggil Jeong <sgjeong1108@icloud.com>
    Co-authored-by: Seonggil Jeong <63443366+seon99il@users.noreply.github.com>
    Co-authored-by: Kim, Min-guk <74689888+minguk-cucu@users.noreply.github.com>
    Co-authored-by: SeongJin LEE <86252835+seongjin33hj@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,24 @@ COPY dbt_requirements.txt .
 RUN uv venv /opt/airflow/dbt_venv && \
     uv pip install --python /opt/airflow/dbt_venv -r dbt_requirements.txt
 
+RUN uv pip install --system \
+    --extra-index-url https://download.pytorch.org/whl/cpu \
+    torch torchvision
 
 RUN uv pip install --system -r pyproject.toml
+
+RUN apt-get update && apt-get install -y \
+    mecab \
+    libmecab-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN ln -s /etc/mecabrc /usr/local/etc/mecabrc
 
 RUN playwright install-deps chromium
 
 USER airflow
 RUN playwright install chromium
 
+RUN pip install transformers -U
+RUN uv pip install --no-cache "spacy>=3.7.0,<4.0.0"
+RUN python -m spacy download en_core_web_sm
---
+++ src/dags/extract_tag_dag.py
@@ -0,0 +1,121 @@
+import gc
+import json
+import logging
+import time
+
+import pandas as pd
+from airflow.sdk import DAG, task
+from extractor.tag_extractor import generate_multilang_tags
+from hooks.s3_hook import S3Hook
+from hooks.snowflake_analytics_hook import SnowflakeAnalyticsQueryHook
+from hooks.snowflake_command_hook import SnowflakeCommandHook
+
+logger = logging.getLogger(__name__)
+
+@task
+def get_urls_without_tags(limit:int = 200, **context) -> str:
+    """Tag가 없는 대상 목록 가져오기"""
+    snowflake_hook = SnowflakeAnalyticsQueryHook()
+    link_id_without_tags = snowflake_hook.get_urls_without_tags()[:limit]
+
+    s3hook = S3Hook(bucket_name='de7-team1')
+    ds_nodash = context['ds_nodash']
+    bytes_data = json.dumps(link_id_without_tags).encode('utf-8')
+    tmp_key_path = f'tmp/link_id_without_tags_{ds_nodash}.json'
+
+    s3hook.upload_bytes(
+        bytes_data=bytes_data,
+        key=tmp_key_path,
+        replace=True
+    )
+
+    logger.info(
+        f'{len(link_id_without_tags)} links looking for tags'
+    )
+    logger.info(f'tmp file saved on: {tmp_key_path}')
+    return tmp_key_path
+
+@task
+def extract_tags(chunk_size:int = 100, **context):
+    """Tag 추출"""
+    s3hook = S3Hook(bucket_name='de7-team1')
+    ti = context['ti']
+    tmp_key_path = ti.xcom_pull(task_ids='get_urls_without_tags')
+    json_string = s3hook.download_bytes(tmp_key_path)
+    link_id_without_tags = json.loads(json_string)
+
+    title_desc_df = pd.DataFrame(
+        link_id_without_tags,
+        columns=['link_id', 'title', 'description']
+        )
+
+    link_id_with_tags = []
+
+    logger.info(f'Start extracting tags for {len(title_desc_df)} links')
+    for i in range(0, len(title_desc_df), chunk_size):
+        try:
+            df = generate_multilang_tags(title_desc_df[i:i + chunk_size])
+            link_id_with_tags.append(df)
+            logger.info(f'Processed chunk index: {i} ~ {i + chunk_size}')
+        except Exception as e:
+            logger.error(f'Error processing chunk starting at index {i}: {e}')
+            continue
+
+        gc.collect()
+
+    df = pd.concat(link_id_with_tags, ignore_index=True)
+    link_id_with_tags = df[['link_id', 'tags']]
+
+    s3hook = S3Hook(bucket_name='de7-team1')
+    ds_nodash = context['ds_nodash']
+    bytes_data = json.dumps(link_id_with_tags.to_json(orient='records')).encode('utf-8')
+    tmp_key_path = f'tmp/link_id_with_tags_{ds_nodash}.json'
+
+    s3hook.upload_bytes(
+        bytes_data=bytes_data,
+        key=tmp_key_path,
+        replace=True
+    )
+    logger.info(
+        f'{len(link_id_with_tags)} links waiting for update'
+    )
+    logger.info(f'tmp file saved on: {tmp_key_path}')
+    return tmp_key_path
+
+@task
+def save_tags(**context):
+    """최종 테이블 업데이트"""
+    s3hook = S3Hook(bucket_name='de7-team1')
+    ti = context['ti']
+    tmp_key_path = ti.xcom_pull(task_ids='extract_tags')
+    json_string = s3hook.download_bytes(tmp_key_path)
+    link_id_with_tags = json.loads(json_string)
+
+    if isinstance(link_id_with_tags, str):
+        link_id_with_tags = json.loads(link_id_with_tags)
+
+    df = pd.DataFrame(link_id_with_tags)
+    exploded_df_list = df.explode('tags').values.tolist()
+
+    sql = """
+        INSERT INTO LINKCHAIN.ANALYTICS.TAG(LINK_ID, TAG_NAME)
+        VALUES (%s, %s)
+    """
+    hook = SnowflakeCommandHook()
+    start = time.time()
+    with hook.get_conn() as conn:
+        with conn.cursor() as cursor:
+            cursor.executemany(sql, exploded_df_list)
+            updated_count = cursor.rowcount
+            conn.commit()
+            logger.info(f"Successfully updated {updated_count} rows.")
+            logger.info(f'execute time : {time.time() - start:.2f}s')
+            return updated_count
+
+with DAG(
+    dag_id="extract_tag_dag",
+    schedule="@daily",
+    start_date=None,
+    catchup=False,
+) as dag:
+    get_urls_without_tags() >> extract_tags() >> save_tags()
